3dc5475c949dbf3aef9a08051fb4f323ec69dea0
John Paul M. Curada <84607933+JpCurada@users.noreply.github.com>
2022-05-16 20:30:43 +0800

    Update PH_RGDP_Dashboard.py

--- PH_RGDP_Dashboard.py
+++ PH_RGDP_Dashboard.py
@@ -196,14 +196,6 @@ st.download_button(
      mime='text/csv',
  )
 
-# --- HIDE STREAMLIT STYLE ---
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
  
 
